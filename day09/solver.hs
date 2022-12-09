50b1c092cf977724bae0e35556f94011db832562
David Koch <dave-koch@web.de>
2022-12-09 19:51:47 +0100

    day 9 part 2 in haskell

--- day09/solver.hs
+++ day09/solver.hs
@@ -6,7 +6,9 @@ import Utils
 import Geometry
 import qualified Data.Vector as Vec
 import Data.Vector (Vector)
+import qualified Data.Set as Set
 import qualified Data.Map as Map
+import Data.Set (Set)
 import AoC
 
 getInput :: IO String
@@ -14,44 +16,40 @@ getInput = readFile "input.txt"
 
 --------------------------------------------------------------------
 
-type Parsed = [Move]
+type Parsed = [Direction]
 type Sol1 = Int
 type Sol2 = Int
 
-data Direction = D | U | R | L deriving (Read, Show)
+type Direction = Vector Int
 data Move = Move Direction Int deriving (Show)
 type Position = Vector Int
-data Rope = Rope { headPos :: Position, tailPos :: Position } deriving (Show)
+-- represents one segment of a rope
+data Segment = Segment { headPos :: Position, tailPos :: Position } deriving (Show)
 
 steps :: [Move] -> [Direction]
 steps = concat . map stepify
     where stepify (Move d n) = take n $ repeat d
 
 parse :: String -> Parsed
-parse = linesWith parseLine
+parse = steps . linesWith parseLine
     where
-        parseLine line = let [dir,dist] = words line in Move (read dir) (read dist)
+        parseLine line = let [dir,dist] = words line in Move (translate dir) (read dist)
+        translate d = case d of
+            "D" -> mkVec [0, -1]
+            "U" -> mkVec [0, 1]
+            "R" -> mkVec [1, 0]
+            "L" -> mkVec [-1, 0]
 
-left, right, up, down :: Position -> Position
-left = vadd (mkVec [-1, 0])
-right = vadd (mkVec [1, 0])
-up = vadd (mkVec [0, 1])
-down = vadd (mkVec [0, -1])
-
--- how the rope moves one step in a direction
+-- how one segment of the rope moves one step in a direction
 -- one step is important because we need to keep track
--- of the position for each step
-moveRope :: Direction -> Rope -> Rope
-moveRope dir rope = Rope headpos' tailpos'
+-- of the position for each step (do not do R 4 at once but rather RRRR)
+moveSegment :: Direction -> Segment -> Segment
+moveSegment dir segment = Segment headpos' tailpos'
     where
-        headpos = headPos rope
+        headpos = headPos segment
         -- the head just moves according to the direction
-        headpos' = case dir of
-            D -> down headpos
-            U -> up headpos
-            L -> left headpos
-            R -> right headpos
-        tailpos' = follow headpos' (tailPos rope)
+        headpos' = vadd dir headpos
+        tailpos' = follow headpos' (tailPos segment)
 
 follow :: Position -> Position -> Position
 follow newhead tail = 
@@ -64,43 +62,50 @@ follow newhead tail =
     where
         direction = vsub newhead tail
 
-startrope :: Rope
-startrope = Rope (mkVec [0,0]) (mkVec [0,0])
+startsegment :: Segment
+startsegment = Segment (mkVec [0,0]) (mkVec [0,0])
 
 -- keep track of where the tail was
-tailtrail :: Grid Int Int
-tailtrail = Map.singleton (mkVec [0,0]) 1
+tailtrail :: Set Position
+tailtrail = Set.singleton (mkVec [0,0])
 
-keeptrack :: Position -> Grid Int Int -> Grid Int Int
-keeptrack p trail = 
-    if p `Map.member` trail then
-        Map.adjust (+1) p trail
-    else
-        Map.insert p 1 trail
+type Rope = [Segment]
 
-type RopeState = (Grid Int Int, Rope)
+moveRope :: Direction -> Rope -> Rope
+moveRope d (h:tls) = h' : follows h' tls
+    where h' = moveSegment d h
+
+-- make the rope follow the rope segment
+follows :: Segment -> Rope -> Rope
+follows _ [] = []
+follows h (t:ts) = t' : follows t' ts
+    where
+        t' = moveSegment dir t
+        dir = Vec.map signum $ vsub (tailPos h) (headPos t)
+
+type RopeState = (Set Position, Rope)
 
 update :: RopeState -> Direction -> RopeState
-update (trail, rope) d = (keeptrack (tailPos rope') trail, rope')
+update (trail, rope) d = (Set.insert (tailPos (last rope')) trail, rope')
     where
         rope' = moveRope d rope
 
+solve :: Rope -> Parsed -> RopeState
+solve startrope = foldl update (tailtrail, startrope)
+
 solve1 :: Parsed -> Sol1
-solve1 = Map.size . fst . foldl update (tailtrail, startrope) . steps
-
-type Longrope = [Rope]
-
-moveLongrope :: Direction -> Longrope -> Longrope
-moveLongrope d (h:tls) = h' : follows h' tls
-
-follows :: Rope -> [Rope] -> [Rope]
-follows _ [] = []
-follows h (t:ts) = t' : follows t' ts
-    where t' = follow (tailPos h) (headPos t) -- does not work, tail does not follow
+solve1 = Set.size . fst . solve [startsegment]
 
 solve2 :: Parsed -> Sol2
-solve2 parsed = undefined
+-- take 9 $ repeat startrope because a ropelist of 9 segments has 10 knots
+solve2 = Set.size . fst . solve (take 9 $ repeat startsegment)
 
+-- in ghci, do
+-- > parsed <- parse <$> getInput
+-- > let (trail, ropes) = solve (take 9 $ repeat startsegment) parsed
+-- > visualize trail
+visualize :: Set Position -> IO ()
+visualize trail = putStr . Geometry.prettyShow "." . Map.fromList $ zipWith (,) (Set.toList trail) (repeat "#")
 
 testdata = [r|R 4
 U 4
@@ -110,13 +115,21 @@ R 4
 D 1
 L 5
 R 2|]
+testdata2 = [r|R 5
+U 8
+L 8
+D 3
+R 17
+D 10
+L 25
+U 20|]
 testresult1 = 13
-testresult2 = 0
+testresult2 = 36
 
 --------------------------------------------------------------------
 
 test1 = test (solve1 . parse) testresult1 testdata
-test2 = test (solve2 . parse) testresult2 testdata
+test2 = test (solve2 . parse) testresult2 testdata2
 
 printPart :: (Show a) => Int -> a -> IO ()
 printPart part solution = do
